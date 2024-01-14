4a5bb04bc329be9380d42c464c9b78d40564aeee
curtcurt69 <cecrouse96@yahoo.com>
2024-01-14 01:19:42 -0500

    creates the edit and delete functionality and begins the basic layout of the Explore page

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -15,18 +15,21 @@ import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
 import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
-import { useCreatePost } from "@/lib/react-query/queriesandMutations"
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesandMutations"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
 import { useNavigate } from "react-router-dom"
 
 type PostFormProps = {
   post?: Models.Document;
+  action: 'Create' | 'Update';
 }
 
-const PostForm = ({ post }: PostFormProps) => {
+const PostForm = ({ post, action }: PostFormProps) => {
 
     const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+    const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost();
+
     const { user } = useUserContext();
     const { toast } = useToast();
     const navigate = useNavigate();
@@ -43,6 +46,22 @@ const PostForm = ({ post }: PostFormProps) => {
      
       // 2. Define a submit handler.
       async function onSubmit(values: z.infer<typeof PostValidation>) {
+        if (post && action === 'Update') {
+          const updatedPost = await updatePost({
+            ...values,
+            postId: post.$id,
+            imageId: post?.imageId,
+            imageUrl: post?.imageUrl
+          });
+
+          if (!updatedPost) {
+            toast({
+              title: 'Please try again!',
+            });
+          }
+
+          return navigate(`/posts/${post.$id}`)
+        }
         const newPost = await createPost({
           ...values,
           userId: user.id,
@@ -57,7 +76,7 @@ const PostForm = ({ post }: PostFormProps) => {
         navigate('/');
       }
 
-
+      
   return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
@@ -118,14 +137,17 @@ const PostForm = ({ post }: PostFormProps) => {
         <Button 
             type="button" 
             className="shad-button_dark_4"
+            onClick={() => navigate('/')}
         >
             Cancel
         </Button>
         <Button 
             type="submit"
             className="shad-button_primary whitespace-nowrap"
+            disabled={isLoadingCreate || isLoadingUpdate}
         >
-            Submit
+            {isLoadingCreate || isLoadingUpdate && 'Loading...'}
+            {action} Post
         </Button>
       </div>
       
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-import { INewPost, INewUser } from "@/types";
+import { INewPost, INewUser, IUpdatePost } from "@/types";
 import { account, appwriteConfig, avatars, databases, storage } from "./config";
 import { ID } from "appwrite";
 import { Query } from "appwrite";
@@ -243,6 +243,85 @@ export async function createPost(post: INewPost) {
       if (!statusCode) throw Error;
 
       return { status: 'Ok' };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export async function getPostById(postId: string) {
+    try {
+      const post = await databases.getDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        postId
+      );
+
+      return post;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export async function updatePost(post: IUpdatePost) {
+    const hasFileToUpdate = post.file.length > 0;
+
+    try {
+      let image = {
+        imageUrl: post.imageUrl,
+        imageId: post.imageId
+      }
+
+      if (hasFileToUpdate) {
+        const uploadedFile = await uploadFile(post.file[0]);
+        if (!uploadedFile) throw Error;
+
+        const fileUrl = getFilePreview(uploadedFile.$id);
+        if (!fileUrl) {
+          await deleteFile(uploadedFile.$id);
+            throw Error;
+        }
+
+        image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id }
+      }
+
+      const tags = post.tags?.replace(/ /g, "").split(",") || [];
+  
+      // Create post
+      const updatedPost = await databases.updateDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        post.postId,
+        {
+          caption: post.caption,
+          imageUrl: image.imageUrl,
+          imageId: image.imageId,
+          location: post.location,
+          tags: tags,
+        }
+      );
+    
+      if (!updatedPost) {
+        await deleteFile(post.imageId);
+        throw Error;
+      }
+    
+      return updatedPost;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export async function deletePost(postId?: string, imageId?: string) {
+    if (!postId || !imageId) throw Error;
+
+    try {
+      await databases.deleteDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        postId
+      );
+
+      return { status: 'Ok'};
     } catch (error) {
       console.log(error);
     }
